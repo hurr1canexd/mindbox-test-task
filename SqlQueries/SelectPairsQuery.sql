e9bed8df04ac7a38c3163f8a09b2a5d1498c3936
unknown <mikal99@mail.ru>
2021-03-04 20:11:45 +0300

    ansi->utf-8

--- SqlQueries/SelectPairsQuery.sql
+++ SqlQueries/SelectPairsQuery.sql
@@ -1,4 +1,4 @@
--- ¬ыбор всех пар Ђ»м€ продукта Ц »м€ категорииї
+-- Выбор всех пар "Имя продукта - Имя категории"
 SELECT Product.ProductName, Category.CategoryName 
 FROM Product
 LEFT JOIN Category ON Product.CategoryId = Category.CategoryId;
